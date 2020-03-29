49a14a9e23dab1e7f4b43fff5190ad5d78f9a790
rodrigommrtz <62777546+rodrigommrtz@users.noreply.github.com>
2020-03-29 19:04:53 -0300

    Add files via upload

---
+++ 04_Lista1.2_Ex30_funcao_e_Procedimento/src/main/java/Ex30_Procedimento_Funcao.java
@@ -0,0 +1,162 @@
+/* Objetivo: Receba a data de nascimento e atual em ano, mês e dia. Calcule e 
+ *           mostre a idade em anos, meses e dias, considerando os anos bissextos.
+ *
+ * Programador: Rodrigo Martinez
+ * Data: 26/Fev/2020
+ */
+
+import javax.swing.JOptionPane;
+
+public class Ex30_Procedimento_Funcao 
+{
+    public static void main (String args [])
+    {
+        int ano0, mes0, dia0;
+        int ano1, mes1, dia1;
+        
+        dia0 = Integer.parseInt (JOptionPane.showInputDialog ("Digite o Dia de Nascimento"));
+        mes0 = Integer.parseInt (JOptionPane.showInputDialog ("Digite o MÊS de Nascimento"));
+        ano0 = Integer.parseInt (JOptionPane.showInputDialog ("Digite o ANO de Nascimento"));
+        
+        dia1 = Integer.parseInt (JOptionPane.showInputDialog ("Digite o Dia atual"));
+        mes1 = Integer.parseInt (JOptionPane.showInputDialog ("Digite o MÊS atual"));
+        ano1 = Integer.parseInt (JOptionPane.showInputDialog ("Digite o ANO atual"));
+        
+        procedimentoAno (ano0, ano1, mes0, mes1, dia0, dia1);
+        
+    }
+
+        static void procedimentoAno(int ano0, int ano1, int mes0, int mes1, int dia0, int dia1)
+        {
+        
+        int anoT, mesT, diaT;
+        int difdias, bis=0; 
+            
+        funcaoBissexto(ano0, ano1, mes0, mes1, dia0, dia1);
+        
+        if (mes0 == 2)
+        {
+            difdias = (28 + bis) - dia0;
+            
+        }else
+        {
+            if (mes0 == 1 || mes0 == 3 || mes0 == 5 || mes0 == 7 || mes0 == 8 || mes0 == 10 | mes0 == 12)
+            {
+                difdias = 31 - dia0;
+            }else
+            {
+                difdias = 30 - dia0;
+            }
+        }
+        
+        // anos
+        if (ano1 == ano0 && mes0 == mes1)
+        {
+            anoT = ano0 - ano1;
+            
+        }else
+        {
+            if (ano1 == ano0 + 1)
+            {
+                anoT = ano1 - ano0;
+            }else
+            {
+                anoT = ano1 - (ano0 + 1);
+            }
+        }
+                
+        // meses
+        if (ano1 == ano0)
+        {
+            mesT = mes1 - mes0;
+        }
+        
+        if (mes0 == 1)
+        {
+            mesT = mes0 - mes1;
+        }else
+        {
+            if (ano1 == ano0)
+            {
+                mesT = mes1 - mes0;
+            }else
+            {
+                mesT = (12 - mes0) + mes1;
+            }
+        }
+
+        if (mesT > 12)
+        {
+            mesT = mesT - 12;
+            anoT = anoT + 1;
+        }
+       
+        //dias
+        diaT = difdias + dia1;
+        
+        //ajustes
+        if (diaT > 28) 
+        {
+            if (mes1 ==1 || mes1 == 3 || mes1 == 5 || mes1 == 7 || mes1 == 8 || mes1==10 || mes1 == 12)
+            {
+                diaT = diaT - 31;
+            }
+        }else
+        {
+            if (mes1 == 4 || mes1 == 6 || mes1 == 9 || mes1 == 11)
+            {
+                diaT = diaT - 30;
+            }else
+            {
+                diaT = diaT - (28 + bis);
+            }
+        }
+        
+        System.out.println ("Sua idade é: " + anoT + " anos " + mesT + " meses e " + diaT + " dias");
+                
+        }
+    
+    
+    
+    static int funcaoBissexto(int ano0, int ano1, int mes0, int mes1, int dia0, int dia1)
+    {    
+    
+        int count, bis=0;
+          
+        count = ano0;
+        if (ano1 == ano0)
+        {
+            while (count == ano1)
+            {
+                if (ano1 % 4 == 0 || ano1 % 400 == 0)
+                {
+                    bis = bis + 1;
+                    count = count - 1;
+                }else
+                {
+                    bis = bis + 0;
+                    count = count + 1;
+                }
+            }
+        } else
+        {
+            while (count < ano1)
+            {
+                if (ano1 % 4 == 0 || ano1 % 400 == 0)
+                {
+                    bis = bis +1;
+                    count = count + 1;
+                } else
+                {
+                     bis = bis + 0;
+                     count = count + 1;
+                }
+            }
+        }
+        
+        return bis;
+        
+    }
+      
+        
+}
